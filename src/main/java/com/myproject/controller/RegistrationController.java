8ca9dd8ec1939bdf0b064aeb8e7ff8064430105a
DaxxHappinezz <dev.mrseo3o3@gmail.com>
2024-01-06 03:50:15 +0900

    add from mini pc

--- src/main/java/com/myproject/controller/RegistrationController.java
+++ src/main/java/com/myproject/controller/RegistrationController.java
@@ -2,12 +2,13 @@ package com.myproject.controller;
 
 import com.myproject.domain.User;
 import com.myproject.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/account")
@@ -38,5 +39,23 @@ public class RegistrationController {
         }
         // 2. 홈 or 이전 페이지로 이동
         return "redirect:/";
+    }
+
+    @PostMapping("/checkId")
+    @ResponseBody
+    public ResponseEntity<String> checkId(@RequestParam("id") String id) {
+//    public ResponseEntity<String> checkId(@RequestBody String id) {
+        try {
+            System.out.println("id = " + id);
+            User user = this.userService.getUser(id);
+            System.out.println("user = " + user);
+            if (user != null) throw new Exception("User id exists.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest()
+                    .body("CHK_ERR");
+        }
+        return ResponseEntity.ok()
+                .body("CHK_OK");
     }
 }
